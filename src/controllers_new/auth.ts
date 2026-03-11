1c3119d0c65d9f71fecf11a0845a3d7a2ac1797f
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-03-11 20:52:27 +0100

    feat: add auth controller and service(testing)

---
+++ src/controllers_new/auth.ts
@@ -0,0 +1,32 @@
+import type express from "express";
+import { UserLoginSchema, UserRegisterSchema } from "schemas/user";
+import type { AuthenticationService } from "services_new/auth";
+import { asyncHandler, HttpError } from "utils/errorHandler";
+
+export class AuthenticationController {
+  constructor(private authService: AuthenticationService) {}
+
+  register = asyncHandler(async (req: express.Request, res: express.Response) => {
+    const data = UserRegisterSchema.parse(req.body);
+    const result = await this.authService.register(data);
+    res.status(200).json(result).end();
+  });
+
+  login = asyncHandler(async (req: express.Request, res: express.Response) => {
+    const data = UserLoginSchema.parse(req.body);
+    const token = await this.authService.login(data);
+    res.cookie("token", token, { httpOnly: true, maxAge: 60 * 60 * 1000 });
+    res.status(200).json("Success!").end();
+  });
+
+  logout = asyncHandler(async (req: express.Request, res: express.Response) => {
+    res.clearCookie("token", { httpOnly: true });
+    res.status(200).json({ message: "Logged out successfully" }).end();
+  });
+
+  whoami = asyncHandler(async (req: express.Request, res: express.Response) => {
+    if (!req.userId) throw new HttpError(401, `Invalid data, you must provide userId`);
+    const result = this.authService.whoami(req.userId);
+    res.status(200).json(result);
+  });
+}
---
+++ src/services_new/auth.ts
@@ -0,0 +1,64 @@
+import type { AuthenticationRepository } from "repositories/auth";
+import type { UserRepository } from "repositories/users";
+import type { AuthenticationInsert } from "schemas/authentication";
+import { UserFromRow, type UserLogin, type UserRegister } from "schemas/user";
+import { authentication, random } from "utils/authentication";
+import { HttpError } from "utils/errorHandler";
+import jwt from "jsonwebtoken";
+
+export class AuthenticationService {
+  constructor(private authRepository: AuthenticationRepository, private userRepository: UserRepository) {}
+
+  async register(data: UserRegister) {
+    const { user, password } = data
+    if (!user.code && !user.username) throw new HttpError(401, "You must provide code or username");
+
+    const { code, username } = user;
+
+    const existingUser = code ? 
+      await this.userRepository.find({ code }) : 
+      username &&
+      await this.userRepository.find({ username });
+
+    if (existingUser) throw new HttpError(409, "User already exists");
+
+    const addedUser = await this.userRepository.create(user)
+
+    const salt = random()
+    const hash = authentication(salt, password)
+
+    const authObject: AuthenticationInsert = { userId: addedUser.id, salt, hash}
+
+    await this.authRepository.create(authObject);
+
+    return UserFromRow.parse(addedUser);
+  }
+
+  async login(data: UserLogin) {
+    const { user, password } = data
+    if (!user.code && !user.username) throw new HttpError(401, "You must provide code or username");
+
+    const { code, username } = user;
+
+    const existingUser = code ? 
+      await this.userRepository.find({ code }) : 
+      username &&
+      await this.userRepository.find({ username });
+
+    if (!existingUser) throw new HttpError(401, "User is not found")
+
+    const auth = await this.authRepository.find(existingUser.id);
+    const expectedHash = authentication(auth.salt, password);
+
+    if(expectedHash !== auth.hash) throw new HttpError(401, "Invalid credentials");
+
+    const token = jwt.sign({ userId: existingUser.id }, process.env.JWT_SECRET!, { expiresIn: "1h" });
+    return token;
+  }
+
+  async whoami(id: number) {
+    const user = await this.userRepository.find({ id })
+    if (!user) throw new HttpError(404, `User with ID ${id} not found`)
+    return user;
+  }
+}
